1bedaaf87e31c9fa68042ca7be38f157f6451e8e
Anthony Buisset <abuisset@gmail.com>
2023-08-17 19:22:10 +0300

    default light and dark themes

--- src/components/Background.tsx
+++ src/components/Background.tsx
@@ -1,5 +1,7 @@
 import { PropsWithChildren } from "react";
 
 export const Background = ({ children }: PropsWithChildren) => (
-  <div className="h-full min-h-screen w-full bg-gradient-to-br from-slate-800 to-slate-900">{children}</div>
+  <div className="h-full min-h-screen w-full bg-gradient-to-br from-slate-100 to-slate-200 dark:from-slate-800 dark:to-slate-900">
+    {children}
+  </div>
 );
