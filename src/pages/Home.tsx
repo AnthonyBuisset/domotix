cb8d6606015483aa1c8c09676cb26b64aa9ad911
Anthony Buisset <abuisset@gmail.com>
2023-08-19 00:21:01 +0300

    restore clock component in homepage

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,31 @@
 import { WeatherForecast } from "../components/WeatherForecast";
+import { useNow } from "../hooks/useNow";
+import { useWeatherForecast } from "../hooks/useWeatherForecast";
 
 export const Home = () => {
   return (
-    <div className="p-2">
+    <div className="flex flex-col items-center gap-8 p-2">
+      <Clock />
       <WeatherForecast />
+    </div>
+  );
+};
+
+const Clock = () => {
+  const { weekday, date, hours, minutes, seconds } = useNow();
+  const forecast = useWeatherForecast();
+
+  return (
+    <div className=" my-2 flex flex-col items-center gap-1">
+      {forecast && <img src={forecast?.current.weather[0].icon} className="w-20" />}
+      <div>
+        {weekday} {date}
+      </div>
+      <div className="flex items-end gap-1">
+        <div className="text-4xl font-semibold">{hours}</div>
+        <div className="text-2xl">{minutes}</div>
+        <div className="text-2xl text-secondary">{seconds}</div>
+      </div>
     </div>
   );
 };
