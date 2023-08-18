985051ca6ad23d59b110ba284085a7a2d6c327ee
Anthony Buisset <abuisset@gmail.com>
2023-08-19 00:42:26 +0300

    dark theme based on OS

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,16 +1,12 @@
 import { Outlet } from "react-router-dom";
 import { Navigation } from "./Navigation";
 import { Background } from "../Background";
-import { DarkThemeToggle, Flowbite } from "flowbite-react";
 
 export const Layout = () => {
   return (
-    <Flowbite>
-      <DarkThemeToggle className="fixed right-4 top-4 z-50 h-10 w-10" />
-      <Background>
-        <Outlet />
-        <Navigation />
-      </Background>
-    </Flowbite>
+    <Background>
+      <Outlet />
+      <Navigation />
+    </Background>
   );
 };
