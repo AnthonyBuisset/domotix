38e79f1248097b56cdbb24cad32b8502e99025a5
Anthony Buisset <abuisset@gmail.com>
2023-08-19 23:15:54 +0300

    responsive weather forecast

--- src/components/WeatherForecast.tsx
+++ src/components/WeatherForecast.tsx
@@ -12,31 +12,37 @@ export const WeatherForecast = () => {
   const forecast = useWeatherForecast();
 
   return (
-    <div className="flex flex-row gap-2">
+    <div className="flex w-full flex-col gap-4 sm:w-fit md:grid md:grid-cols-2 lg:flex lg:flex-row">
       {forecast?.daily.slice(1).map((f, index) => {
         const { weekday, day, month } = format(f.date);
         return (
-          <Card key={f.dt} className="flex flex-col items-center">
-            <div>{`${weekday} ${day} ${month}`}</div>
+          <Card key={f.dt} className="flex flex-row items-center justify-between gap-4 lg:flex-col">
+            <div className="flex flex-col items-center gap-1 lg:flex-row">
+              <span>{weekday}</span>
+              <span>{day}</span>
+              <span>{month}</span>
+            </div>
             <img src={f.weather[0].icon} className="w-14" />
-            <div className="flex flex-row items-center">
-              <img
-                src={
-                  f.temp.day < forecast.daily[index].temp.day
-                    ? thermometerCooler
-                    : f.temp.day > forecast.daily[index].temp.day
-                    ? thermometerWarmer
-                    : thermometer
-                }
-                className="w-8"
-              />
-              {f.temp.day.toFixed(1)} °C
+            <div className="flex flex-col items-center gap-2">
+              <div className="flex flex-row items-center">
+                <img
+                  src={
+                    f.temp.day < forecast.daily[index].temp.day
+                      ? thermometerCooler
+                      : f.temp.day > forecast.daily[index].temp.day
+                      ? thermometerWarmer
+                      : thermometer
+                  }
+                  className="w-8"
+                />
+                {f.temp.day.toFixed(1)} °C
+              </div>
+              <div className="flex flex-row items-center">
+                <img src={wind} className="w-8" />
+                {(f.wind_speed * 3.6).toFixed(0)} km/h
+              </div>
             </div>
-            <div className="flex flex-row items-center">
-              <img src={wind} className="w-8" />
-              {(f.wind_speed * 3.6).toFixed(0)} km/h
-            </div>
-            <img src={UV_INDEXES[Math.round(f.uvi) - 1]} className="w-10" />
+            <img src={UV_INDEXES[Math.round(f.uvi) - 1]} className="w-14" />
           </Card>
         );
       })}
