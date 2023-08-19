9fd45cc2d1b1ff41645d8a0b9af673af0aa7b428
Anthony Buisset <abuisset@gmail.com>
2023-08-19 23:15:08 +0300

    fix config

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
+import "flowbite";
 import { Connector } from "mqtt-react-hooks";
 
 import config from "./config.ts";
