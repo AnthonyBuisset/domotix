b7bf7b34e9ceb12c696c9bead24b5c8fa1d1823a
Anthony Buisset <abuisset@gmail.com>
2023-08-20 19:17:15 +0300

    fix dark mode theme

--- src/main.tsx
+++ src/main.tsx
@@ -1,20 +1,21 @@
+import "./index.css";
+
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
-import "flowbite";
+import { BrowserRouter as Router } from "react-router-dom";
 import { Connector } from "mqtt-react-hooks";
-
 import config from "./config.ts";
 import { App } from "./App.tsx";
-
-import { BrowserRouter as Router } from "react-router-dom";
+import { Flowbite } from "flowbite-react";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <Connector brokerUrl={config.MQTT_BROKER_URL}>
-      <Router>
-        <App />
-      </Router>
-    </Connector>
+    <Flowbite>
+      <Connector brokerUrl={config.MQTT_BROKER_URL}>
+        <Router>
+          <App />
+        </Router>
+      </Connector>
+    </Flowbite>
   </React.StrictMode>
 );
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
+import flowbite from "flowbite/plugin";
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}", "node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}"],
+  darkMode: "media",
   theme: {
     extend: {
       textColor: theme => ({
@@ -15,5 +18,5 @@ export default {
       },
     },
   },
-  plugins: [import("flowbite/plugin")],
+  plugins: [flowbite],
 };
