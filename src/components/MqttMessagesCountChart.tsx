8a7ef3240edf1d00dabc3eddb04241a1268886b5
Anthony Buisset <abuisset@gmail.com>
2023-08-25 23:38:05 +0300

    extract useInfluxDbQuery hook

--- src/components/MqttMessagesCountChart.tsx
+++ src/components/MqttMessagesCountChart.tsx
@@ -1,9 +1,5 @@
-import { InfluxDB } from "@influxdata/influxdb-client-browser";
-
-import config from "../config";
-import { useAlert } from "../hooks/useAlert";
 import { Bar, BarChart, ResponsiveContainer, Tooltip, XAxis, YAxis } from "recharts";
-import { useEffect, useState } from "react";
+import useInfluxDbQuery from "../hooks/useInfluxDbQuery";
 
 const QUERY = `
 from(bucket: "mqtt-messages")
@@ -15,43 +11,20 @@ from(bucket: "mqtt-messages")
   |> limit(n:10)
 `;
 
-const API = new InfluxDB({ url: config.INFLUXDB_URL, token: config.INFLUXDB_API_TOKEN }).getQueryApi(
-  config.INFLUXDB_ORG
-);
-
 type Stat = {
   topic: string;
-  count: number;
+  _value: number;
 };
 
 export default function MqttMessagesCountChart() {
-  const { alert } = useAlert();
-  const [data, setData] = useState<Stat[]>();
-
-  useEffect(() => {
-    const rows: Stat[] = [];
-    API.queryRows(QUERY, {
-      next(row, tableMeta) {
-        rows.push({
-          topic: tableMeta.get(row, "topic"),
-          count: tableMeta.get(row, "_value"),
-        });
-      },
-      complete() {
-        setData(rows);
-      },
-      error(error) {
-        alert(error.toString());
-      },
-    });
-  }, []);
+  const { data } = useInfluxDbQuery<Stat>(QUERY);
 
   return (
     <ResponsiveContainer minWidth={200} height={200}>
-      <BarChart layout="vertical" data={data} margin={{ top: 30, right: 5, left: 5 }} barSize={16}>
+      <BarChart layout="vertical" data={data} barSize={16}>
         <XAxis type="number" tickCount={2} />
         <YAxis type="category" width={150} dataKey="topic" tickLine={false} axisLine={false} />
-        <Bar layout="vertical" dataKey="count" fill="#8884d8" />
+        <Bar layout="vertical" dataKey="_value" fill="#8884d8" />
         <Tooltip />
       </BarChart>
     </ResponsiveContainer>
---
+++ src/hooks/useInfluxDbQuery.ts
@@ -0,0 +1,32 @@
+import { InfluxDB } from "@influxdata/influxdb-client-browser";
+import { useEffect, useState } from "react";
+import config from "../config";
+
+const API = new InfluxDB({ url: config.INFLUXDB_URL, token: config.INFLUXDB_API_TOKEN }).getQueryApi(
+  config.INFLUXDB_ORG
+);
+
+export default function useInfluxDbQuery<R>(query: string): { data?: R[]; loading: boolean } {
+  const [data, setData] = useState<R[]>();
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const rows: R[] = [];
+    setLoading(true);
+    API.queryRows(query, {
+      next(row, tableMeta) {
+        rows.push(tableMeta.toObject(row) as R);
+      },
+      complete() {
+        setData(rows);
+        setLoading(false);
+      },
+      error(error) {
+        setLoading(false);
+        alert(error.toString());
+      },
+    });
+  }, []);
+
+  return { data, loading };
+}
