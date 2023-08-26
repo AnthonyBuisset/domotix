4d5bfa3845782b56935a84b63c8508a5ab641652
Anthony Buisset <abuisset@gmail.com>
2023-08-26 23:22:17 +0300

    all messages graph

---
+++ src/components/MqttMessagesChart.tsx
@@ -0,0 +1,104 @@
+import { Bar, ComposedChart, Line, ResponsiveContainer, Tooltip, XAxis, YAxis } from "recharts";
+import useInfluxDbQuery, { Range } from "../hooks/useInfluxDbQuery";
+import { Card } from "./Card";
+import { flux, fluxDuration } from "@influxdata/influxdb-client-browser";
+import { useState } from "react";
+import Dropdown from "./Dropdown";
+import { chain } from "lodash";
+
+type Stat = {
+  _time: string;
+  _value: number;
+  topic: string;
+};
+
+const OPTIONS: Record<Range, string> = {
+  [Range.LastHour]: "1 heure",
+  [Range.Last4Hours]: "4 heures",
+  [Range.LastDay]: "1 jour",
+  [Range.LastWeek]: "1 semaine",
+};
+
+const TIME_INTERVALS: Record<Range, string> = {
+  [Range.LastHour]: "10m",
+  [Range.Last4Hours]: "30m",
+  [Range.LastDay]: "1h",
+  [Range.LastWeek]: "12h",
+};
+
+const DATETIME_FORMATS: Record<Range, Intl.DateTimeFormatOptions> = {
+  [Range.LastHour]: { timeStyle: "short" },
+  [Range.Last4Hours]: { timeStyle: "short" },
+  [Range.LastDay]: { timeStyle: "short" },
+  [Range.LastWeek]: { day: "2-digit", month: "short" },
+};
+
+const COLORS = [
+  "#0ea5e9", // sky-500
+  "#a855f7", // purple-500
+  "#10b981", // emerald-500
+  "#ef4444", // red-500
+  "#fde047", // yellow-500
+  "#f59e0b", // amber-500
+  "#ec4899", // pink-500
+  "#64748b", // slate-500
+];
+
+export default function MqttMessagesChart() {
+  const [range, setRange] = useState<Range>(Range.LastDay);
+
+  const formatDate = (date: string) =>
+    new Intl.DateTimeFormat(undefined, DATETIME_FORMATS[range]).format(new Date(date));
+
+  const start = fluxDuration(range);
+  const every = fluxDuration(TIME_INTERVALS[range]);
+  const query = flux`
+  from(bucket: "mqtt-messages")
+    |> range(start: ${start})
+    |> filter(fn: (r) => r._measurement == "messages" and r._field == "topic" )
+    |> window(every: ${every})
+    |> count()
+    |> duplicate(column: "_stop", as: "_time")
+  `;
+
+  const { data } = useInfluxDbQuery<Stat>(query.toString());
+
+  const values = Object.entries(
+    data?.reduce(
+      (acc, msg) => ({
+        ...acc,
+        [msg._time]: {
+          ...acc[msg._time],
+          [msg.topic]: msg._value,
+          total: (acc[msg._time]?.total || 0) + msg._value,
+        },
+      }),
+      {} as Record<string, Record<string, number>>
+    ) || {}
+  ).map(([time, topics]) => ({ time, ...topics }));
+
+  const topics = chain(data).map("topic").sort().uniq().value();
+
+  return (
+    <Card title="MQTT messages" className="relative border">
+      <div className="absolute right-4 top-4">
+        <Dropdown
+          options={Object.entries(OPTIONS).map(([value, label]) => ({ value, label }))}
+          label={OPTIONS[range]}
+          onChange={option => setRange(option as Range)}
+        />
+      </div>
+      <ResponsiveContainer minWidth={200} height={300}>
+        <ComposedChart data={values} maxBarSize={16}>
+          <XAxis type="category" dataKey="time" tickFormatter={formatDate} />
+          <YAxis type="number" />
+          {topics.map((topic, i) => (
+            <Bar key={topic} dataKey={topic} fill={COLORS[i]} />
+          ))}
+          <Line dataKey="total" stroke="#ffffff" accumulate="sum" />
+          <Tooltip />
+        </ComposedChart>
+      </ResponsiveContainer>
+    </Card>
+  );
+}
--- src/pages/Monitoring/RaspberryPi.tsx
+++ src/pages/Monitoring/RaspberryPi.tsx
@@ -1,9 +1,11 @@
+import MqttMessagesChart from "../../components/MqttMessagesChart";
 import MqttMessagesCountChart from "../../components/MqttMessagesCountChart";
 
 export const RaspberryPi = () => {
   return (
     <div className="flex h-fit w-full grid-cols-2 flex-col gap-2 p-2 md:grid">
       <MqttMessagesCountChart />
+      <MqttMessagesChart />
     </div>
   );
 };
