7e46317b05547a324c3d95f5aab58135a83e1e77
Anthony Buisset <abuisset@gmail.com>
2023-08-30 00:42:15 +0200

    connect slider to MQTT
    more rooms

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -1,20 +1,38 @@
 import { RangeSlider } from "flowbite-react";
-import { ReactElement, useState } from "react";
+import { ReactElement } from "react";
+import { useJsonMqttValues, useMqttClient } from "../hooks/useMqtt";
+import { useDebouncedCallback } from "use-debounce";
 
 type Props = {
+  topic: string;
   offIcon: ReactElement;
   onIcon: ReactElement;
 };
 
-export function Slider({ offIcon, onIcon }: Props) {
-  const [value, setValue] = useState(50);
+export function Slider({ topic, offIcon, onIcon }: Props) {
+  const [brightness, state] = useJsonMqttValues({ topic, paths: ["$.brightness_l1", "$.state_l1"] });
+  const percent = parseInt(state === "OFF" ? "0" : brightness) / 2.55;
+
+  const client = useMqttClient();
+  const setValue = (percent: number) =>
+    client?.publish(topic + "/set", JSON.stringify({ brightness_l1: (percent * 2.55).toFixed() }));
+
+  const debouncedSetValue = useDebouncedCallback(setValue, 300);
 
   return (
     <div className="flex w-full flex-row items-center gap-4">
-      <div className="cursor-pointer" onClick={() => setValue(value ? 0 : 100)}>
-        {value ? onIcon : offIcon}
+      <div className="cursor-pointer" onClick={() => setValue(brightness ? 0 : 100)}>
+        {brightness ? (
+          <button onClick={() => setValue(0)}>{onIcon}</button>
+        ) : (
+          <button onClick={() => setValue(100)}>{offIcon}</button>
+        )}
       </div>
-      <RangeSlider className="w-full" value={value} onChange={({ target }) => setValue(parseInt(target.value))} />{" "}
+      <RangeSlider
+        className="w-full"
+        value={percent.toString()}
+        onChange={({ target }) => debouncedSetValue(parseInt(target.value))}
+      />
     </div>
   );
 }
--- src/pages/Home/Indoor.tsx
+++ src/pages/Home/Indoor.tsx
@@ -9,22 +9,31 @@ import { ToggleSwitch } from "../../components/ToggleSwitch";
 import { Open as ShutterOpen, Closed as ShutterClosed } from "../../icons/Shutters";
 
 export const Indoor = () => (
-  <div className="flex h-fit w-full grid-cols-4 flex-col gap-2 p-2 sm:grid">
+  <div className="flex h-fit w-full grid-cols-2 flex-col gap-2 p-2 sm:grid lg:grid-cols-3 xl:grid-cols-4">
     <Card icon={<RiHotelBedFill />} title="Chambre parentale" className="col-span-2">
-      <div className="flex gap-2">
+      <div className="flex flex-col gap-2 sm:flex-row">
         <THB topic="zigbee2mqtt/parental-bedroom/thb" />
-        <div className="flex w-1/2 shrink-0 flex-col gap-4">
+        <div className="flex shrink-0 flex-col gap-4 sm:w-1/2">
           <Slider
             onIcon={<RiLightbulbFill className="text-3xl" />}
             offIcon={<RiLightbulbLine className="text-3xl" />}
+            topic="zigbee2mqtt/parental-bedroom/light"
           />
           <div className="flex items-center gap-1">
             <RiDoorOpenFill className="text-3xl" />
-            <Slider onIcon={<ShutterOpen size={30} />} offIcon={<ShutterClosed size={30} />} />
+            <Slider
+              topic="zigbee2mqtt/parental-bedroom/curtain/door"
+              onIcon={<ShutterOpen size={30} />}
+              offIcon={<ShutterClosed size={30} />}
+            />
           </div>
           <div className="flex items-center gap-1">
             <TbWindow className="text-3xl" />
-            <Slider onIcon={<ShutterOpen size={30} />} offIcon={<ShutterClosed size={30} />} />
+            <Slider
+              topic="zigbee2mqtt/parental-bedroom/curtain/window"
+              onIcon={<ShutterOpen size={30} />}
+              offIcon={<ShutterClosed size={30} />}
+            />
           </div>
           <ToggleSwitch icon={<RiPlugFill className="text-3xl" />} />
         </div>
