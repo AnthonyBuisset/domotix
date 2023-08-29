f92e01c4f58cce58356f3684b6f7b381d03d4a15
Anthony Buisset <abuisset@gmail.com>
2023-08-29 23:15:46 +0200

    more rooms

--- src/pages/Home/Indoor.tsx
+++ src/pages/Home/Indoor.tsx
@@ -7,27 +7,41 @@ import { ToggleSwitch } from "../../components/ToggleSwitch";
 import { Open as ShutterOpen, Closed as ShutterClosed } from "../../icons/Shutters";
 
 export const Indoor = () => (
-  <div className="flex h-fit w-full grid-cols-2 flex-col gap-2 p-2 sm:grid">
-    <Card title="Chambre parentale" className="row-span-2">
-      <div className="flex flex-col gap-4">
-        <THB />
-        <Slider onIcon={<RiLightbulbFill className="text-3xl" />} offIcon={<RiLightbulbLine className="text-3xl" />} />
-        <div className="flex items-center gap-1">
-          <RiDoorOpenFill className="text-3xl" />
-          <Slider onIcon={<ShutterOpen size={30} />} offIcon={<ShutterClosed size={30} />} />
+  <div className="flex h-fit w-full grid-cols-4 flex-col gap-2 p-2 sm:grid">
+    <Card title="Chambre parentale" className="col-span-2">
+      <div className="flex gap-2">
+        <THB topic="zigbee2mqtt/parental-bedroom/thb" />
+        <div className="flex w-1/2 shrink-0 flex-col gap-4">
+          <Slider
+            onIcon={<RiLightbulbFill className="text-3xl" />}
+            offIcon={<RiLightbulbLine className="text-3xl" />}
+          />
+          <div className="flex items-center gap-1">
+            <RiDoorOpenFill className="text-3xl" />
+            <Slider onIcon={<ShutterOpen size={30} />} offIcon={<ShutterClosed size={30} />} />
+          </div>
+          <div className="flex items-center gap-1">
+            <TbWindow className="text-3xl" />
+            <Slider onIcon={<ShutterOpen size={30} />} offIcon={<ShutterClosed size={30} />} />
+          </div>
+          <ToggleSwitch icon={<RiPlugFill className="text-3xl" />} />
         </div>
-        <div className="flex items-center gap-1">
-          <TbWindow className="text-3xl" />
-          <Slider onIcon={<ShutterOpen size={30} />} offIcon={<ShutterClosed size={30} />} />
-        </div>
-        <ToggleSwitch icon={<RiPlugFill className="text-3xl" />} />
       </div>
     </Card>
     <Card title="Chambre de Christophe">
-      <THB />
+      <THB topic="" />
     </Card>
     <Card title="Chambre de Marie">
-      <THB />
+      <THB topic="" />
+    </Card>
+    <Card title="Bureau">
+      <THB topic="zigbee2mqtt/office/thb" />
+    </Card>
+    <Card title="Salon">
+      <THB topic="zigbee2mqtt/living/thb" />
+    </Card>
+    <Card title="Salle a manger">
+      <THB topic="zigbee2mqtt/dining/thb" />
     </Card>
   </div>
 );
