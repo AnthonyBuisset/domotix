5873744d6647991e8335af14c14f45b88fe3df55
Anthony Buisset <abuisset@gmail.com>
2023-08-31 22:47:28 +0200

    add link quality in dimmer

--- src/components/Dimmer.tsx
+++ src/components/Dimmer.tsx
@@ -1,6 +1,7 @@
 import { RiLightbulbFill, RiLightbulbLine } from "react-icons/ri";
 import { useJsonMqttValues, useMqttClient } from "../hooks/useMqtt";
 import { Slider } from "./Slider";
+import { Card } from "./Card";
 
 type Props = {
   topic: string;
@@ -8,18 +9,23 @@ type Props = {
 
 export function Dimmer({ topic }: Props) {
   const client = useMqttClient();
-  const [brightness, state] = useJsonMqttValues({ topic, paths: ["$.brightness_l1", "$.state_l1"] });
+  const [brightness, state, linkquality] = useJsonMqttValues({
+    topic,
+    paths: ["$.brightness_l1", "$.state_l1", "$.linkquality"],
+  });
 
   const publishBrightness = (brightness: number) =>
     client?.publish(`${topic}/set`, JSON.stringify({ brightness_l1: brightness, state_l1: brightness ? "ON" : "OFF" }));
 
   return (
-    <Slider
-      value={parseInt(state === "OFF" ? "0" : brightness)}
-      setValue={publishBrightness}
-      onIcon={<RiLightbulbFill className="text-2xl" />}
-      offIcon={<RiLightbulbLine className="text-2xl" />}
-      max={255}
-    />
+    <Card linkquality={linkquality} title="Lumieres">
+      <Slider
+        value={parseInt(state === "OFF" ? "0" : brightness)}
+        setValue={publishBrightness}
+        onIcon={<RiLightbulbFill className="text-2xl" />}
+        offIcon={<RiLightbulbLine className="text-2xl" />}
+        max={255}
+      />
+    </Card>
   );
 }
