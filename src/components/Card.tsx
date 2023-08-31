8592c08555725b88f252c9eb0ea0034b9d4d1e78
Anthony Buisset <abuisset@gmail.com>
2023-08-31 22:41:32 +0200

    move linkquality and battery display to Card

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,20 +1,93 @@
 import { PropsWithChildren, ReactElement } from "react";
-import { Card as View } from "flowbite-react";
-
+import { Tooltip } from "flowbite-react";
+import {
+  RiBatteryFill,
+  RiBatteryLine,
+  RiBatteryLowLine,
+  RiSignalWifi1Fill,
+  RiSignalWifi2Fill,
+  RiSignalWifi3Fill,
+  RiSignalWifiErrorLine,
+  RiSignalWifiFill,
+} from "react-icons/ri";
+import classNames from "classnames";
 interface Props extends PropsWithChildren {
   title?: string;
   icon?: ReactElement;
   className?: string;
+  linkquality?: string | number;
+  battery?: string | number;
 }
 
-export const Card = ({ title, icon, className, children }: Props) => (
-  <View className={className}>
+export const Card = ({ title, icon, className, linkquality, battery, children }: Props) => (
+  <div
+    className={classNames(
+      "relative flex flex-col gap-2 rounded-lg border border-gray-300 bg-gray-200 p-4 shadow dark:border-gray-700 dark:bg-white/[0.02]",
+      className
+    )}
+  >
+    <div className="absolute right-2 top-2 flex items-center gap-2">
+      {linkquality !== undefined && (
+        <Tooltip content={`${linkquality}%`}>
+          <LinkQuality value={toNumber(linkquality)} />
+        </Tooltip>
+      )}
+      {battery !== undefined && (
+        <Tooltip content={`${battery}%`}>
+          <Battery value={toNumber(battery)} />
+        </Tooltip>
+      )}
+    </div>
     {(title || icon) && (
       <div className="flex items-center gap-2 text-xl text-black dark:text-gray-200">
         {icon}
         <h1>{title}</h1>
       </div>
     )}
-    <div className="h-full">{children}</div>
-  </View>
+    {children}
+  </div>
 );
+
+type InnerProps = {
+  value: number;
+};
+
+const toNumber = (value: number | string): number => (typeof value === "string" ? parseFloat(value) : value);
+
+const LinkQuality = ({ value }: InnerProps) =>
+  value > 80 ? (
+    <div>
+      <RiSignalWifiFill />
+    </div>
+  ) : value > 60 ? (
+    <div>
+      <RiSignalWifi3Fill />
+    </div>
+  ) : value > 40 ? (
+    <div className="text-yellow-500">
+      <RiSignalWifi2Fill />
+    </div>
+  ) : value > 10 ? (
+    <div className="text-yellow-500">
+      <RiSignalWifi1Fill />
+    </div>
+  ) : (
+    <div className="text-red-600">
+      <RiSignalWifiErrorLine />
+    </div>
+  );
+
+const Battery = ({ value }: InnerProps) =>
+  value > 50 ? (
+    <div>
+      <RiBatteryFill />
+    </div>
+  ) : value > 20 ? (
+    <div className="text-yellow-500">
+      <RiBatteryLowLine />
+    </div>
+  ) : (
+    <div className="text-red-600">
+      <RiBatteryLine />
+    </div>
+  );
--- src/components/THB.tsx
+++ src/components/THB.tsx
@@ -2,18 +2,8 @@ import thermometer from "/assets/weather-icons/fill/thermometer.svg";
 import humidity from "/assets/weather-icons/fill/humidity.svg";
 import barometer from "/assets/weather-icons/fill/barometer.svg";
 import { useJsonMqttValues } from "../hooks/useMqtt";
-import {
-  RiBatteryFill,
-  RiBatteryLine,
-  RiBatteryLowLine,
-  RiSignalWifi1Fill,
-  RiSignalWifi2Fill,
-  RiSignalWifi3Fill,
-  RiSignalWifiErrorLine,
-  RiSignalWifiFill,
-} from "react-icons/ri";
+
 import { Card } from "./Card";
-import { Tooltip } from "flowbite-react";
 
 type Props = {
   topic: string;
@@ -26,20 +16,12 @@ export const THB = ({ topic }: Props) => {
   });
 
   return (
-    <Card className="w-full">
+    <Card className="w-full" linkquality={linkquality} battery={battery}>
       <div className="flex gap-2">
         <div className="flex grow flex-col">
           <Temperature value={parseFloat(temperature)} />
           <Humidity value={parseFloat(humidity)} />
           <Barometer value={parseFloat(barometer)} />
-        </div>
-        <div className="flex items-center gap-2 self-start">
-          <Tooltip content={`${linkquality}%`}>
-            <LinkQuality value={parseFloat(linkquality)} />
-          </Tooltip>
-          <Tooltip content={`${battery}%`}>
-            <Battery value={parseFloat(battery)} />
-          </Tooltip>
         </div>
       </div>
     </Card>
@@ -70,41 +52,3 @@ const Barometer = ({ value }: InnerProps) => (
     <p>{value.toFixed(0)} hPa</p>
   </div>
 );
-
-const LinkQuality = ({ value }: InnerProps) =>
-  value > 80 ? (
-    <div>
-      <RiSignalWifiFill />
-    </div>
-  ) : value > 60 ? (
-    <div>
-      <RiSignalWifi3Fill />
-    </div>
-  ) : value > 40 ? (
-    <div className="text-yellow-500">
-      <RiSignalWifi2Fill />
-    </div>
-  ) : value > 10 ? (
-    <div className="text-yellow-500">
-      <RiSignalWifi1Fill />
-    </div>
-  ) : (
-    <div className="text-red-600">
-      <RiSignalWifiErrorLine />
-    </div>
-  );
-
-const Battery = ({ value }: InnerProps) =>
-  value > 50 ? (
-    <div>
-      <RiBatteryFill />
-    </div>
-  ) : value > 20 ? (
-    <div className="text-yellow-500">
-      <RiBatteryLowLine />
-    </div>
-  ) : (
-    <div className="text-red-600">
-      <RiBatteryLine />
-    </div>
-  );
