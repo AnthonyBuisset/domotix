046394e38226554edf7d87bd735d9fd4a96e826f
Anthony Buisset <abuisset@gmail.com>
2023-09-02 23:38:45 +0200

    new gauge component for temperatures

--- src/components/THB.tsx
+++ src/components/THB.tsx
@@ -2,7 +2,7 @@ import thermometer from "/assets/weather-icons/fill/thermometer.svg";
 import humidity from "/assets/weather-icons/fill/humidity.svg";
 import barometer from "/assets/weather-icons/fill/barometer.svg";
 import { useJsonMqttValues } from "../hooks/useMqtt";
-
+import GaugeComponent from "react-gauge-component";
 import { Card } from "./Card";
 
 type Props = {
@@ -19,7 +19,7 @@ export default function THB({ topic }: Props) {
     <Card className="w-full" linkquality={linkquality} battery={battery}>
       <div className="flex gap-2">
         <div className="flex grow flex-col">
-          <Temperature value={parseFloat(temperature)} />
+          {temperature !== undefined && <Temperature value={parseFloat(temperature)} />}
           <Humidity value={parseFloat(humidity)} />
           <Barometer value={parseFloat(barometer)} />
         </div>
@@ -32,12 +32,38 @@ type InnerProps = {
   value: number;
 };
 
-const Temperature = ({ value }: InnerProps) => (
-  <div className="flex items-center gap-1">
-    <img src={thermometer} className="-mx-2 h-10" />
-    <p>{value.toFixed(1)} °C</p>
-  </div>
-);
+const Temperature = ({ value }: InnerProps) => {
+  const min = -10;
+  const max = 40;
+  const nbSubArcs = (max - min) * 10;
+
+  return (
+    <div className="relative">
+      <img
+        src={thermometer}
+        className="absolute left-1/2 top-1/2 m-auto h-2/3 w-2/3 -translate-x-1/2 -translate-y-1/2"
+      />
+      <GaugeComponent
+        type="radial"
+        arc={{
+          colorArray: ["#38bdf8", "#ea580c"],
+          padding: 0,
+          width: 0.05,
+          subArcs: [...Array(nbSubArcs + 1).keys()].map(x => ({ limit: (x * (max - min)) / nbSubArcs + min })),
+          cornerRadius: 0,
+        }}
+        pointer={{ type: "blob", width: 12 }}
+        labels={{
+          valueLabel: { formatTextValue: value => value + "ºC", maxDecimalDigits: 1, matchColorWithArc: true },
+          tickLabels: { hideMinMax: true },
+        }}
+        value={Math.max(min, Math.min(max, value))}
+        minValue={min}
+        maxValue={max}
+      />
+    </div>
+  );
+};
 
 const Humidity = ({ value }: InnerProps) => (
   <div className="flex items-center gap-1">
