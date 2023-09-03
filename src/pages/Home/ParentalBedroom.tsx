3c0ae978ec13aa547c4e3d93ca136c15fb01b628
Anthony Buisset <abuisset@gmail.com>
2023-09-03 21:33:08 +0200

    responsive home page

--- src/pages/Home/ParentalBedroom.tsx
+++ src/pages/Home/ParentalBedroom.tsx
@@ -6,11 +6,13 @@ import { CurtainSwitch } from "../../components/CurtainSwitch";
 export default function ParentalBedroom() {
   return (
     <>
-      <THB className="lg:row-span-2" topic="zigbee2mqtt/parental-bedroom/thb" />
-      <Dimmer title="Lumieres" topic="zigbee2mqtt/parental-bedroom/light" />
-      <CurtainSwitch title="Porte" topic="zigbee2mqtt/parental-bedroom/curtain/door" />
-      <CurtainSwitch title="Fenetre" topic="zigbee2mqtt/parental-bedroom/curtain/window" />
-      <Socket title="Coiffeuse" topic="zigbee2mqtt/parental-bedroom/socket" />
+      <THB topic="zigbee2mqtt/parental-bedroom/thb" />
+      <div className="flex grow flex-col gap-2 lg:grid lg:grid-cols-2 xl:grid-cols-3 2xl:h-fit 2xl:grid-cols-4">
+        <Dimmer title="Lumieres" topic="zigbee2mqtt/parental-bedroom/light" />
+        <CurtainSwitch title="Porte" topic="zigbee2mqtt/parental-bedroom/curtain/door" />
+        <CurtainSwitch title="Fenetre" topic="zigbee2mqtt/parental-bedroom/curtain/window" />
+        <Socket title="Coiffeuse" topic="zigbee2mqtt/parental-bedroom/socket" />
+      </div>
     </>
   );
 }
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -3,9 +3,9 @@ import { Outlet } from "react-router-dom";
 
 export const Home = () => {
   return (
-    <div className="flex h-full flex-col">
+    <div className="flex h-full flex-col md:flex-row">
       <Sidebar />
-      <div className="mb-2 flex h-full grid-cols-2 grid-rows-6 flex-col gap-2 overflow-y-auto px-2">
+      <div className="flex h-full w-full flex-col gap-2 overflow-y-auto px-2 pb-2 md:p-2 lg:h-fit lg:flex-row">
         <Outlet />
       </div>
     </div>
