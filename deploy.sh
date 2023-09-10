bceb7ebffbf62d6d901b584231460bb6407bdb5a
Anthony Buisset <abuisset@gmail.com>
2023-09-10 23:17:36 +0200

    update deploy for Dad

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,21 @@
 #!/bin/bash
 
 yarn build
-scp -r dist/* pi@home.local:/var/www/html
+
+if [ $? -ne 0 ]
+then
+    echo Build error
+    exit 1
+fi
+
+if [ `ping -t 1 -c 1 home.local > /dev/null`]
+then
+    # Antho
+    TARGET=pi@home.local:/var/www/html
+else
+    # Papa
+    TARGET=bb@192.168.1.222:/var/www/html
+fi
+
+scp -r dist/* $TARGET
+
