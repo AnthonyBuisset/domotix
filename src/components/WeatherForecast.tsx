77bd7c04dd1a60404a6d0b05bfc9756963242d32
Anthony Buisset <abuisset@gmail.com>
2024-04-24 21:49:55 +0200

    remove uv index

--- src/components/WeatherForecast.tsx
+++ src/components/WeatherForecast.tsx
@@ -6,8 +6,6 @@ import thermometerWarmer from "/assets/weather-icons/fill/thermometer-warmer.svg
 import thermometerCooler from "/assets/weather-icons/fill/thermometer-colder.svg";
 import wind from "/assets/weather-icons/fill/wind.svg";
 
-const UV_INDEXES = [...Array(11).keys()].map(i => `/assets/weather-icons/fill/uv-index-${i + 1}.svg`);
-
 export const WeatherForecast = () => {
   const forecast = useWeatherForecast();
 
@@ -24,26 +22,23 @@ export const WeatherForecast = () => {
                 <span>{month}</span>
               </div>
               <img src={f.weather[0].icon} className="w-14" />
-              <div className="flex flex-col items-center gap-2 sm:flex-row lg:flex-col">
-                <div className="flex flex-row items-center">
-                  <img
-                    src={
-                      f.temp.day < forecast.daily[index].temp.day
-                        ? thermometerCooler
-                        : f.temp.day > forecast.daily[index].temp.day
-                        ? thermometerWarmer
-                        : thermometer
-                    }
-                    className="w-8"
-                  />
-                  {f.temp.day.toFixed(1)} °C
-                </div>
-                <div className="flex flex-row items-center">
-                  <img src={wind} className="w-8" />
-                  {(f.wind_speed * 3.6).toFixed(0)} km/h
-                </div>
+              <div className="flex flex-row items-center">
+                <img
+                  src={
+                    f.temp.day < forecast.daily[index].temp.day
+                      ? thermometerCooler
+                      : f.temp.day > forecast.daily[index].temp.day
+                      ? thermometerWarmer
+                      : thermometer
+                  }
+                  className="w-8"
+                />
+                {f.temp.day.toFixed(1)} °C
               </div>
-              <img src={UV_INDEXES[Math.round(f.uvi) - 1]} className="w-14" />
+              <div className="flex flex-row items-center">
+                <img src={wind} className="w-8" />
+                {(f.wind_speed * 3.6).toFixed(0)} km/h
+              </div>
             </div>
           </Card>
         );
